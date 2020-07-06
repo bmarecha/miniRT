2c25c4ad2b0b6c0778eacd5922528a24ca5e7467
Baptiste MARECHAL <bmarecha@e3r2p23.42.fr>
2020-07-06 18:56:33 +0200

    infos structures

--- minifiles/miniRT.h
+++ minifiles/miniRT.h
@@ -6,13 +6,45 @@
 /*   By: bmarecha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/06 17:27:17 by bmarecha          #+#    #+#             */
-/*   Updated: 2020/07/06 17:30:40 by bmarecha         ###   ########.fr       */
+/*   Updated: 2020/07/06 18:55:38 by bmarecha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINIRT_H
 # define  MINIRT_H
 # include <stdio.h>
+
+typedef struct	s_point
+{
+	float x;
+	float y;
+	float z;
+}				t_point;
+
+typedef	struct s_colors
+{
+	int r;
+	int g;
+	int b;
+}				t_colors;
+
+typedef struct	s_camera
+{
+	t_point place;
+	t_point view;
+
+	int fov;
+}				t_camera;
+
+typedef	struct	s_scene
+{
+	int xsize;
+	int ysize;
+	float ambiantr;
+	t_colors ambiantc;
+
+	t_camera *cams;
+}				t_scene;
 
 void printtest(void);
 
