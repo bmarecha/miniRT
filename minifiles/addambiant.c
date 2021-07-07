d861d4e0617954db33e151e7e72a3a782757caec
BM <baptistemarechal42@gmail.com>
2021-07-07 14:15:20 +0200

    norm ori cam

--- minifiles/addambiant.c
+++ minifiles/addambiant.c
@@ -80,8 +80,7 @@ int	add_camera(char **infos, t_scene *scene)
 	point_create(infos[4], infos[5], infos[6], &(new->view));
 	new->fov = ft_atoi(infos[7]);
 	new->base = get_cam_space(new->view);
-	if (!(new->fov) || new->view.x * new->view.x > 1
-		|| new->view.y * new->view.y > 1 || new->view.z * new->view.z > 1)
+	if (!(new->fov) || normv(new->view) > 1 || !normv(new->view))
 	{
 		free(new);
 		return (-1);
